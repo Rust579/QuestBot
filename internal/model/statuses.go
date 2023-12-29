af7fc5fa8b836e68c786c993c7d5a4a4890219a6
Rust579 <rustem3494@gmail.com>
2023-12-29 21:43:05 +0500

    Обновленные пулы

--- internal/model/statuses.go
+++ internal/model/statuses.go
@@ -1,26 +1,21 @@
 package model
 
 import (
+	"errors"
 	"sync"
 )
 
-var PullUsers = Pull{P: map[int64]User{}}
+var PullUsers = Pull{P: map[string]int64{}}
 
 type Pull struct {
 	sync.Mutex
-	P     map[int64]User
+	P     map[string]int64
 	Stage int
-}
-
-type User struct {
-	ChatId int64
-	Name   string
-	//Team   int
 }
 
 func (p *Pull) AddUser(chatId int64, name string, stage int) {
 	p.Lock()
-	p.P[chatId] = User{ChatId: chatId, Name: name}
+	p.P[name] = chatId
 	p.Stage = stage
 	p.Unlock()
 }
@@ -31,8 +26,24 @@ func (p *Pull) IncStage(stage int) {
 	p.Unlock()
 }
 
-func (p *Pull) GetUser(chatId int64) User {
+func (p *Pull) GetUser(name string) (int64, error) {
 	p.Lock()
 	defer p.Unlock()
-	return p.P[chatId]
+	usId, ok := p.P[name]
+	if !ok {
+		return 0, errors.New("Нету юзера")
+	}
+	return usId, nil
+}
+
+func (p *Pull) GetAllUserIds() []int64 {
+
+	p.Lock()
+	defer p.Unlock()
+
+	var userIds []int64
+	for _, u := range p.P {
+		userIds = append(userIds, u)
+	}
+	return userIds
 }
